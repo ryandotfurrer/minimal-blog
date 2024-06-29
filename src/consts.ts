23ca391b6845fe7116033fbd2d4b94f7b5813388
Ryan Furrer <ryandotfurrer@gmail.com>
2024-06-29 11:06:01 -0400

    Initial styles added

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = 'Minimal Blog';
+export const SITE_DESCRIPTION = 'A minimal blog built on Astro';
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,16 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   // Type-check frontmatter using a schema
   schema: z.object({
-    title: z.string(),
+    author: z.string().optional(),
     description: z.string(),
+    heroImage: z.string().optional(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
+    title: z.string(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
   }),
 });
 
